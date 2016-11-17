1e30750506064cbfcdf7f6e747ddef032dcd8125
AkibShahjahan <akibshahjahan@gmail.com>
2016-11-17 14:41:37 -0500

    Add mlab stuff

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var app = express();
-
+var mongoose = require('mongoose');
+mongoose.connect("mongodb://<pushakib>:<pushakib>@ds029197.mlab.com:29197/sendat")
 
 app.get('/',function(req, res) {
   res.send("<h1>Welcome to Sendat</h1>");
