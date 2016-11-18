00e756da82e630c08d74910123ad24bc62dee0cb
Pushkin Abbott <abbott.pushkin@gmail.com>
2016-11-18 13:07:21 -0500

    lol

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ var express = require('express');
 var app = express();
 var bodyParser = require("body-parser");
 var fs = require('fs');
+<<<<<<< Updated upstream
  var path = require('path');
 
 //bodyParser extracts the body portion of your request and makes its 
@@ -29,7 +30,26 @@ var contentSchema = new mongoose.Schema({
 
 var Content = mongoose.model("Content", contentSchema);
 
-var newpost = new Content({ writing : 'dog' , title : "dog" });
+var mongodb = require('mongodb');
+
+var MongoClient = mongodb.MongoClient;
+
+dispatcher.setStatic('resources');
+
+//Lets define a port we want to listen to
+const PORT=8080; 
+
+//We need a function which handles requests and send response
+function handleRequest(request, response){
+      try {
+        //log the request on console
+        console.log(request.url);
+        //Disptach
+        dispatcher.dispatch(request, response);
+    } catch(err) {
+        console.log(err);
+    }
+}
 
 var PORT = 3000
 
