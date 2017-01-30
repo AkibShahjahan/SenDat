b714a555e91b7c893294612325f53afb054c6e3e
AkibShahjahan <akibshahjahan@gmail.com>
2017-01-30 12:47:04 -0500

    Changed localhost

--- config/auth.js
+++ config/auth.js
@@ -2,6 +2,6 @@ module.exports = {
 	'facebookAuth': {
 		'clientID': '194473814346087',
     	'clientSecret': 'd4d2ca6c678e1578bd823611c612ee36',
-    	'callbackURL': 'http://localhost:3000/auth/facebook/callback'
+    	'callbackURL': 'http://http://ec2-52-201-184-155.compute-1.amazonaws.com:3000/auth/facebook/callback'
 	}
 }
