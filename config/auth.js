3c56502629927b756886b57d6d303f752863db5b
Pushkin Abbott <abbott.pushkin@gmail.com>
2017-02-16 10:34:15 +0530

    changed urls

--- config/auth.js
+++ config/auth.js
@@ -2,6 +2,6 @@ module.exports = {
 	'facebookAuth': {
 		'clientID': '194473814346087',
     	'clientSecret': 'd4d2ca6c678e1578bd823611c612ee36',
-    	'callbackURL': 'http://www.senndat.com/auth/facebook/callback'
+    	'callbackURL': 'http://localhost:3000/auth/facebook/callback'
 	}
 }
