6a35801d4d5d66a6fa6f7b7926fefcaf432c82d4
AkibShahjahan <akibshahjahan@gmail.com>
2017-05-20 16:54:21 -0400

    add senndat url

--- config/auth.js
+++ config/auth.js
@@ -4,6 +4,6 @@ module.exports = {
 		//,'242993122827489',
     	'clientSecret': 'd4d2ca6c678e1578bd823611c612ee36',
     	//'77d0f2c43fd8e983e8a1913f2f6826a1',
-    	'callbackURL': '/auth/facebook/callback'
+    	'callbackURL': 'http://www.senndat.com/auth/facebook/callback'
 	}
 }
