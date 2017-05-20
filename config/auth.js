001e7415d81659cb886c16e1bb62951ca6b0fcd8
AkibShahjahan <akibshahjahan@gmail.com>
2017-05-20 16:45:15 -0400

    Change some stuff

--- config/auth.js
+++ config/auth.js
@@ -1,9 +1,9 @@
 module.exports = {
 	'facebookAuth': {
-		'clientID': '242993122827489',
-		// '194473814346087',
-    	'clientSecret': '77d0f2c43fd8e983e8a1913f2f6826a1',
-    	//'d4d2ca6c678e1578bd823611c612ee36',
+		'clientID': '194473814346087'
+		//,'242993122827489',
+    	'clientSecret': 'd4d2ca6c678e1578bd823611c612ee36',
+    	//'77d0f2c43fd8e983e8a1913f2f6826a1',
     	'callbackURL': '/auth/facebook/callback'
 	}
 }
