d771f2ad87d5ed58efcbd1c5dcc891806b79ada6
Pushkin Abbott <abbott.pushkin@gmail.com>
2017-05-24 13:24:08 -0400

    bro

--- config/auth.js
+++ config/auth.js
@@ -2,6 +2,6 @@ module.exports = {
 	'facebookAuth': {
 		'clientID': '648284028709847',
     	'clientSecret': '193c6ae7aa03aacc79e5fc17f46d1984',
-    	'callbackURL': 'http://localhost:3000/auth/facebook/callback'
+    	'callbackURL': 'http://www.senndat.com/auth/facebook/callback'
 	}
 }
--- config/passport.js
+++ config/passport.js
@@ -21,7 +21,7 @@ module.exports = function(passport) {
 	clientID        : configAuth.facebookAuth.clientID,
     clientSecret    : configAuth.facebookAuth.clientSecret,
     callbackURL     : configAuth.facebookAuth.callbackURL,
-    profileFields		: ['id', 'emails', 'name'],
+    profileFields		: ['id', 'emails', 'name']
 	},
 	function(token, refreshToken, profile, done) {
 		process.nextTick(function() {
